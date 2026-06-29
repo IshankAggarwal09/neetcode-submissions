4a2d5cf40bf8a88239e6abc28a8ecf8c7118fb21
IshankAggarwal09 <aggarwalishu263@gmail.com>
2026-06-29 12:28:50 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isFreqSame(int[] freq1, int[] freq2) {
+        for (int i=0; i<26; i++) {
+            if (freq1[i] != freq2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean checkInclusion(String s1, String s2) {
+        int[] freq = new int[26];
+        for (int i=0; i<s1.length(); i++) {
+            freq[s1.charAt(i) - 'a']++;
+        }
+        int winSize = s1.length();
+        for (int i=0; i<s2.length(); i++) {
+            int winIdx = 0;
+            int idx = i;
+            int[] winFreq = new int[26];
+            while (winIdx < winSize && idx < s2.length()) {
+                winFreq[s2.charAt(idx) - 'a']++;
+                winIdx++;
+                idx++;
+            }
+            if (isFreqSame(freq, winFreq)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
